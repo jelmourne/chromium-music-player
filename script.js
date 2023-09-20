4f6d14c84b87379685b1bec502d3bcc7d455f234
Tristan Dasilva <tristan.dasilva98@gmail.com>
2023-09-20 17:14:45 -0400

    Modification

--- script.js
+++ script.js
@@ -103,15 +103,32 @@ async function getSearch(Query) {
   });
 
   const response = await fetch(`https://api.spotify.com/v1/search?${args}`, {
-    header: {
+    headers: {
       Authorization: "Bearer " + accessToken,
     },
   });
   return await response.json();
 }
 
+// Calls ticketmaster API to fetch events and returns the events as an array
+async function getEvents() {
+  const userProfile = await getProfile();
+  const rootURL = "https://app.ticketmaster.com/discovery/v2/";
+  const apiKey = "MXLBwKzlHC8GwQe6qv9gdnCw2oWr7N3V";
+
+  const response = await fetch(
+    `${rootURL}events.json?classificationName=music&apikey=${apiKey}&countryCode=${userProfile.country}`
+  );
+  const data = await response.json();
+  let events = data._embedded.events;
+
+  return await events;
+}
+
 document
   .getElementById("login-button")
   .addEventListener("click", authorization);
 
+console.log(await getEvents())
 console.log(await getSearch("drake"));
+
