f4a0f2b8f4b3439d8fb51d67fc6539eddc2ab7b0
Justin Elmourne <jelmourne@outlook.com>
2023-10-16 13:03:34 -0400

    Fixed app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-import { authorization } from './authentication.js';
+import { authorization } from "./authentication.js";
 import {
   getProfile,
   getSearch,
@@ -40,88 +40,88 @@ const profile = await getProfile();
 const playlist = await getUserPlaylist();
 
 // Loading dropdown scripts
-var tag = document.createElement('script');
-tag.src = 'https://unpkg.com/flowbite@1.5.1/dist/flowbite.js';
-document.getElementsByTagName('head')[0].appendChild(tag);
+var tag = document.createElement("script");
+tag.src = "https://unpkg.com/flowbite@1.5.1/dist/flowbite.js";
+document.getElementsByTagName("head")[0].appendChild(tag);
 
 // DOM Manipulation Section
 try {
   document
-    .getElementById('login-button')
-    .addEventListener('click', authorization);
+    .getElementById("login-button")
+    .addEventListener("click", authorization);
 } catch (ex) {
   alert(ex);
 }
 
-document.getElementById('profile-name').innerHTML =
-  'Hi, ' + profile.display_name;
+document.getElementById("profile-name").innerHTML =
+  "Hi, " + profile.display_name;
 
-document.getElementById('profile-followers').innerHTML =
-  profile.followers.total + ' Followers';
+document.getElementById("profile-followers").innerHTML =
+  profile.followers.total + " Followers";
 
-document.getElementById('toggle-favs-btn').addEventListener('click', () => {
+document.getElementById("toggle-favs-btn").addEventListener("click", () => {
   showSavedConcerts();
 });
 
-document.getElementById('logout-button').addEventListener('click', () => {
-  localStorage.removeItem('access_token');
+document.getElementById("logout-button").addEventListener("click", () => {
+  localStorage.removeItem("access_token");
   location.reload();
 });
 
 // Spotify player
-var tag = document.createElement('script');
-tag.src = 'https://sdk.scdn.co/spotify-player.js';
-document.getElementsByTagName('body')[0].appendChild(tag);
+var tag = document.createElement("script");
+tag.src = "https://sdk.scdn.co/spotify-player.js";
+document.getElementsByTagName("body")[0].appendChild(tag);
 
-const playButton = document.getElementById('togglePlay');
-const duration = document.getElementById('song-duration');
-const currentTime = document.getElementById('currTime');
+const playButton = document.getElementById("togglePlay");
+const duration = document.getElementById("song-duration");
+const currentTime = document.getElementById("currTime");
 
 // Initializing Web Playback SDK with event listeners
 let genre;
 window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = localStorage.getItem('access_token');
+  const token = localStorage.getItem("access_token");
   const player = new Spotify.Player({
-    name: 'Chrome Extension',
+    name: "Chrome Extension",
     volume: 0.3,
     getOAuthToken: (cb) => {
       cb(token);
     },
   });
 
-  player.addListener('ready', ({ device_id }) => {
+  player.addListener("ready", ({ device_id }) => {
     // const connect_to_device = () => {
-    const accessToken = localStorage.getItem('access_token');
-    console.log('Changing to device');
-    let changeDevice = fetch('https://api.spotify.com/v1/me/player', {
-      method: 'PUT',
+    const accessToken = localStorage.getItem("access_token");
+    console.log("Changing to device");
+    let changeDevice = fetch("https://api.spotify.com/v1/me/player", {
+      method: "PUT",
       body: JSON.stringify({
         device_ids: [device_id],
         play: false,
       }),
       headers: new Headers({
-        Authorization: 'Bearer ' + accessToken,
+        Authorization: "Bearer " + accessToken,
       }),
     });
   });
   // connect_to_device();
   //});
 
-  player.addListener('not_ready', ({ device_id }) => {
-    console.log('Device ID has gone offline', device_id);
+  player.addListener("not_ready", ({ device_id }) => {
+    console.log("Device ID has gone offline", device_id);
   });
 
-  player.addListener('authentication_error', ({ message }) => {
+  player.addListener("authentication_error", ({ message }) => {
     console.error(message);
   });
 
-  player.addListener('account_error', ({ message }) => {
+  player.addListener("account_error", ({ message }) => {
     console.error(message);
   });
 
   player.connect().then((success) => {
     if (success) {
-      console.log('The Web Playback SDK successfully connected to Spotify!');
+      console.log("The Web Playback SDK successfully connected to Spotify!");
     }
   });
 
@@ -136,12 +136,12 @@ window.onSpotifyWebPlaybackSDKReady = () => {
       if (startTime >= endTime) {
         clearTimeout(timer);
         duration.value = 0;
-        currentTime.innerHTML = '0:00';
+        currentTime.innerHTML = "0:00";
       }
     }, 1000);
   }
 
-  player.on('player_state_changed', async (state) => {
+  player.on("player_state_changed", async (state) => {
     let startTime = new Date(state.position);
     let endTime = new Date(state.duration);
 
@@ -153,26 +153,26 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     }
 
     if (!state.paused) {
-      playButton.classList.add('fa-circle-pause');
-      playButton.classList.add('fa-solid');
+      playButton.classList.add("fa-circle-pause");
+      playButton.classList.add("fa-solid");
     } else {
-      playButton.classList.remove('fa-circle-pause');
-      playButton.classList.remove('fa-solid');
+      playButton.classList.remove("fa-circle-pause");
+      playButton.classList.remove("fa-solid");
 
       clearInterval(timer);
     }
 
-    document.getElementById('current-song-name').innerHTML =
+    document.getElementById("current-song-name").innerHTML =
       state.track_window.current_track.name;
 
-    document.getElementById('current-song-img').src =
+    document.getElementById("current-song-img").src =
       state.track_window.current_track.album.images[0].url;
 
-    document.getElementById('current-song-artist').innerHTML =
+    document.getElementById("current-song-artist").innerHTML =
       state.track_window.current_track.artists[0].name;
 
     currentTime.innerHTML = getMinAndSec(state.position);
-    document.getElementById('endTime').innerHTML = getMinAndSec(state.duration);
+    document.getElementById("endTime").innerHTML = getMinAndSec(state.duration);
 
     duration.max = endTime;
     duration.value = startTime;
@@ -185,24 +185,24 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     player.togglePlay();
   };
 
-  document.getElementById('prevTrack').onclick = function () {
+  document.getElementById("prevTrack").onclick = function () {
     player.previousTrack();
   };
 
-  document.getElementById('nextTrack').onclick = function () {
+  document.getElementById("nextTrack").onclick = function () {
     player.nextTrack();
   };
 };
 
 // Toggle suffle
-const sufflePlay = document.getElementById("randomTrack");
+const shufflePlay = document.getElementById("randomTrack");
 shufflePlay.addEventListener("click", () => {
   let params = document.getElementById("randomValue");
-  setSuffle(params.value);
+  setShuffle(params.value);
   if (params.value == "true") {
     params.value = "false";
   } else {
-    params.value = 'true';
+    params.value = "true";
   }
 });
 
@@ -219,13 +219,13 @@ repeatTrack.addEventListener("click", () => {
 });
 
 // Modifies dom with search results from searchbox
-const search = document.getElementById('song-search');
-const searchResults = document.getElementById('search-result');
+const search = document.getElementById("song-search");
+const searchResults = document.getElementById("search-result");
 
 search.addEventListener(
-  'input',
+  "input",
   debounce(() => {
-    searchResults.innerHTML = '';
+    searchResults.innerHTML = "";
     getSearch(search.value).then((response) => {
       searchResults.innerHTML = `<hr><li
         class="flex w-auto bg-white p-2 hover:bg-green-500 hover:text-white transition-all cursor-pointer" onclick="playAlbum('${response.albums.items[0].id}', 'album')"
@@ -277,7 +277,7 @@ search.addEventListener(
 );
 
 // Gets all users playlist and renders them on the page
-const playlistResult = document.getElementById('playlist-dropdown');
+const playlistResult = document.getElementById("playlist-dropdown");
 
 if (playlist.total != 0) {
   playlist.items.forEach((element) => {
@@ -297,8 +297,8 @@ if (playlist.total != 0) {
             <div class="flex flex-row">
               <p>Tracks: ${element.tracks.total}</p>
               <p>&nbsp ${
-                element.owner.display_name == 'undefined'
-                  ? ''
+                element.owner.display_name == "undefined"
+                  ? ""
                   : element.owner.display_name
               }</p>
             </div>
@@ -317,18 +317,19 @@ async function filterConcertsByGenre(artistUrl) {
   showAllConcerts(concerts);
 }
 
-const concertToggle = document.getElementById('concertToggle')
+const concertToggle = document.getElementById("concertToggle");
 
-concertToggle.addEventListener('change', () => {
-  if (document.getElementById('events-container')) {
-    document.getElementById('events-container').remove();
+concertToggle.addEventListener("change", () => {
+  if (document.getElementById("events-container")) {
+    document.getElementById("events-container").remove();
   } else {
-    let eventsContainer = document.createElement('div')
-    eventsContainer.id = 'events-container';
-    eventsContainer.className = 'flex flex-col items-center justify-center overflow-auto h-96';
-    console.log(eventsContainer)
-    document.getElementById('player-container').appendChild(eventsContainer);
-    showAllConcerts(concerts)
+    let eventsContainer = document.createElement("div");
+    eventsContainer.id = "events-container";
+    eventsContainer.className =
+      "flex flex-col items-center justify-center overflow-auto h-96";
+    console.log(eventsContainer);
+    document.getElementById("player-container").appendChild(eventsContainer);
+    showAllConcerts(concerts);
   }
   // document.getElementById('events-container').classList.toggle('invisible')
 });
