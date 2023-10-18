ddb868d37c38726b77e6afacdab305dc56989923
Justin Elmourne <jelmourne@outlook.com>
2023-10-18 11:46:09 -0400

    Final build

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,17 @@ dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   plugins: [],
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
+  build: {
+    target: "esnext",
+  },
   server: {
     host: "127.0.0.1",
     port: 5173,
-    open: '/index.html',
+    open: "/index.html",
   },
 });
